ea3489866cf51a79f145ca16e1c50c1a2fdfa2b1
dee4git <57084424+dee4git@users.noreply.github.com>
2020-01-25 00:00:50 +0600

    Can add stores now

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,6 @@
 
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from. import views
 from django.conf import settings
@@ -14,9 +15,10 @@ urlpatterns = [
     path('mm/ac/', include('accounts.urls')),
     path('acc/', include('accs.urls')),
     path('mm/acc/', include('accs.urls')),
+    path('mm/st/', include('stores.urls')),
 
 ]
-if settings.DEBUG:
-   # urlpatterns = urlpatterns+ static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
-    #urlpatterns = urlpatterns+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
-    pass
+urlpatterns = urlpatterns+ static(settings.STATIC_URL, document_root= settings.STATIC_ROOT)
+urlpatterns = urlpatterns+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns()
+
