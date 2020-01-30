98874b2166499e2332d7b4379901fdc710f9992d
dee4git <57084424+dee4git@users.noreply.github.com>
2020-01-30 15:16:59 +0600

    Bug fixed
    -add store

--- mysite/stores/urls.py
+++ mysite/stores/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from. import views
 urlpatterns = [
     path('add/',views.addStore, name='add_store'),
-    path('showall/',views.shwoStore),
+    path('showallstore/',views.shwoStore),
     path('ursote/',views.urStore),
     path('<int:store_id>/', views.detail, name='detailed_store')
 
